dfd3d7b4144d7f58b2fa36f013a5513a5d6971bc
yejianxin <yejianxin@rightknights.com>
2021-03-25 11:49:51 +0800

    test: udpate the test example

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const js = import("./pkg/hello_world");
+const js = import("./pkg/saltire_decode");
 const data = "3,228,184,173,3,228,184,173,1,97,1,48"
 js.then(js => {
   js.greet("WebAssembly");
-  console.log(js.bar(data))
+  console.log(js.uncode(data))
 });
